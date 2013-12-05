a95457e2925d9f8332cf58e92f64213bda67f7a9
Francis Valois <dark_sz@hotmail.com>
2013-12-04 20:47:17 -0500

    Ajout des figures et des différents fichiers matlab et excel

--- lab7/lab.m
+++ lab7/lab.m
@@ -17,7 +17,7 @@ fc2(2) = fc(3,1);
 fc2(3) = fc(1,2);
 
 
-f = 0:1*10^9:25*10^9;
+f = 10.538*10^9;
 lambda = vp./f;
 lambda_g = zeros(3,26);
 for k = 1:3
@@ -40,3 +40,15 @@ end
 syms x
 test = -0.2739*x^6 + 2.5479*x^5 - 8.6836*x^4 + 13.553*x^3 - 11.174*x^2 + 1.145*x - 30.8136;
 solve(test,x)
+
+
+lambda2 = vp/(10.595*10^9);
+test = 4*pi*17.66666667/1000/lambda2+pi-4*pi
+
+lambda222 = 0.0363820017603311;
+dmin = 0.01767/lambda222;
+beta = 360/lambda222;
+ec1 = 169.52 - 2*beta*dmin+360*26
+ec2 = 169.52 - 2*beta*(dmin+lambda222/4)+360*27
+
+etac1 = 1+
